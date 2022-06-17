ab376666df85de6f661c06d4fd2dfb2eebc933cb
Gabriel Pires de Camargo <72245726+gabrielpirescamargo@users.noreply.github.com>
2022-06-17 09:35:37 -0300

    desafio 02 - events

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-
 new Vue({
-
-    el: '#app',
+    el: '#desafio',
     data: {
-
-        nome: 'gabriel',
-        idade: 18,
-        idadex3: 0,
-        img:'https://www.gannett-cdn.com/-mm-/526c32e3985b7d7a3a026753ab945435cbc0ceb5/c=0-38-768-472/local/-/media/USATODAY/test/2014/01/22/1390420312000-ibm-logo.jpg?width=3200&height=1680&fit=crop'
-        
+        valor: ' '
     },
     methods:{
-        itriplo: function(){
-            this.idadex3 = this.idade*3
-            return this.idadex3
-
+        clicar(){
+            alert("alerta")
         },
-        random: function(){
-            
-            return Math.random();
-
-        },
-
-        imagem: function(){
-            
-            return img;
-
+        tecla(event){
+            this.valor = event.target.value
+           
         }
-
-
-
     }
 })
